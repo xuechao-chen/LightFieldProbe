7eb1437c405a829b5a0e2183cddbacea7acfca5d
voidwalker <xuechao.chen@outlook.com>
2019-10-19 13:05:31 +0800

    refactor

--- LightFiledProbe/App.cpp
+++ LightFiledProbe/App.cpp
@@ -8,110 +8,54 @@ void App::onInit()
 	setFrameDuration(1.0f / 60.f);
 
 	__makeGUI();
-	__createSphereSampler(64);
-	m_pCubemapColor = Texture::createEmpty("Cubemap", 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_CUBE_MAP);
-	m_pCubemapDistance = Texture::createEmpty("CubemapDistance", 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_CUBE_MAP);
 
 	m_gbufferSpecification.encoding[GBuffer::Field::LAMBERTIAN] = ImageFormat::RGB32F();
 	m_gbufferSpecification.encoding[GBuffer::Field::CS_POSITION] = ImageFormat::RGB32F();
 
 	logPrintf("Program initialized\n");
-	//loadScene("G3D Sponza (Glossy)");
-	loadScene("Animated Hardening");
+	loadScene("G3D Sponza (Glossy)");
+	//loadScene("Animated Hardening");
 	logPrintf("Loaded Scene\n");
 
-	AABox BoundingBox;
-	Surface::getBoxBounds(m_posed3D, BoundingBox);
-	__initLightFiledSurface(BoundingBox);
-	__placeProbe();
+	m_SphereSamplerTexture = __createSphereSampler();
+	m_LightFiledSurface = __initLightFiledSurface();
+	m_ProbePositionSet  = __placeProbe(m_LightFiledSurface);
 
-	m_LightFiledFB = Framebuffer::create("LightFiledFB");
-	//GBuffer::Specification GBufferSpec;
-	//GBufferSpec.encoding[GBuffer::Field::CS_POSITION] = ImageFormat::RGB32F();
+	__precomputeLightFiledSurface(m_LightFiledSurface);
 
-	//m_ProbeGBuffer = GBuffer::create(GBufferSpec, "ProbeGBuffer");
-	//m_ProbeGBuffer->resize(Vector2int32(1024, 1024));
-
-
-	//const Vector2int32 framebufferSize = m_settings.hdrFramebuffer.hdrFramebufferSizeFromDeviceSize(Vector2int32(m_deviceFramebuffer->vector2Bounds()));
-	//m_framebuffer->resize(framebufferSize);
-	//m_gbuffer->resize(framebufferSize);
-
-	for (int i = 0; i < 2; ++i)
-	{
-		Array<shared_ptr<Texture>> CubemapTextureColor, CubemapTextureDistance;
-
-		for (int Face = 0; Face < 6; ++Face)
-		{
-			CubemapTextureColor.push(Texture::createEmpty("Cubemap" + Face, 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_2D));
-			CubemapTextureDistance.push(Texture::createEmpty("CubemapDistance" + Face, 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_2D));
-		}
-
-		__renderLightFiledProbe(40 + i, CubemapTextureColor, CubemapTextureDistance);
-
-		for (int Face = 0; Face < 6; ++Face)
-		{
-			Texture::copy(CubemapTextureColor[Face], m_pCubemapColor, 0, 0, 1.0f, Vector2int16(0, 0), CubeFace::POS_X, static_cast<CubeFace>(Face));
-			Texture::copy(CubemapTextureDistance[Face], m_pCubemapDistance, 0, 0, 1.0f, Vector2int16(0, 0), CubeFace::POS_X, static_cast<CubeFace>(Face));
-		}
-
-		m_LightFiledFB->set(Framebuffer::COLOR0, m_LightFiledSurface.RadianceProbeGrid, CubeFace::POS_X, 0, i);
-
-		renderDevice->push2D(m_LightFiledFB); {
-			renderDevice->setBlendFunc(RenderDevice::BLEND_ONE, RenderDevice::BLEND_ZERO);
-			auto Sampler = Sampler::cubeMap();
-			Sampler.interpolateMode = InterpolateMode::BILINEAR_NO_MIPMAP;
-
-			Args args;
-			args.setRect(renderDevice->viewport());
-			args.setUniform("ProbeColorCubemap", m_pCubemapColor, Sampler);
-			args.setUniform("NumSamples", 2048);
-			args.setUniform("LobeSize", 1.0f);
-			args.setUniform("SphereSampler", m_SphereSamplerTexture, Sampler::buffer());
-
-			LAUNCH_SHADER("ComputeProbeGrid.pix", args);
-		} renderDevice->pop2D();
-
-	}
-
-	m_IsInit = true;
+	m_IsPrecomputed = true;
 }
 
-void App::onGraphics3D(RenderDevice* rd, Array<shared_ptr<Surface> >& surface)
+void App::onGraphics3D(RenderDevice* rd, Array<shared_ptr<Surface>>& surface)
 {
-	if (!m_IsInit) { GApp::onGraphics3D(rd, surface); return; }
+	if (!m_IsPrecomputed) { GApp::onGraphics3D(rd, surface); return; }
 
 	rd->swapBuffers();
 	rd->clear();
 
-	//rd->push2D();
-	//{
-	//	rd->setBlendFunc(RenderDevice::BLEND_ONE, RenderDevice::BLEND_ZERO);
-	//	//auto Sampler = Sampler::buffer();
-	//	//Sampler.interpolateMode = InterpolateMode::BILINEAR_NO_MIPMAP;
-
-	//	Args args;
-	//	args.setRect(rd->viewport());
-	//	args.setUniform("Gbuffer", m_gbuffer->texture(GBuffer::Field::CS_POSITION), Sampler::buffer());
-	//	//m_gbuffer->setShaderArgsRead(args, "gbuffer_");
-	//	LAUNCH_SHADER("cubemap.pix", args);
-	//} rd->pop2D();
-
-	rd->push2D(); {
+	rd->push2D();
+	{
 		rd->setBlendFunc(RenderDevice::BLEND_ONE, RenderDevice::BLEND_ZERO);
-		auto Sampler = Sampler::cubeMap();
-		Sampler.interpolateMode = InterpolateMode::BILINEAR_NO_MIPMAP;
 
 		Args args;
 		args.setRect(rd->viewport());
-		args.setUniform("ProbeColorCubemap", m_pCubemapDistance, Sampler);
-		args.setUniform("NumSamples", 2048);
-		args.setUniform("LobeSize", 1.0f);
-		args.setUniform("SphereSampler", m_SphereSamplerTexture, Sampler::buffer());
-
-		LAUNCH_SHADER("ComputeProbeGrid.pix", args);
-
+		args.setUniform("TempArray", m_LightFiledSurface.RadianceProbeGrid, Sampler::buffer());
+		LAUNCH_SHADER("Playground.pix", args);
 	} rd->pop2D();
+
+	//rd->push2D(); {
+	//	rd->setBlendFunc(RenderDevice::BLEND_ONE, RenderDevice::BLEND_ZERO);
+
+	//	Args args;
+	//	args.setRect(rd->viewport());
+	//	args.setUniform("ProbeColorCubemap", m_LightFiledSurface.IrradianceProbeGrid, Sampler::buffer());
+	//	args.setUniform("NumSamples", 2048);
+	//	args.setUniform("LobeSize", 1.0f);
+	//	args.setUniform("SphereSampler", m_SphereSamplerTexture, Sampler::buffer());
+
+	//	LAUNCH_SHADER("ComputeProbeGrid.pix", args);
+
+	//} rd->pop2D();
 
 	//GApp::onGraphics3D(rd, surface);
 	//for (auto i = 0; i < m_ProbePositionSet.size(); ++i)
@@ -120,7 +64,7 @@ void App::onGraphics3D(RenderDevice* rd, Array<shared_ptr<Surface> >& surface)
 	//}
 }
 
-void App::__createSphereSampler(int vDegreeSize)
+shared_ptr<Texture> App::__createSphereSampler(int vDegreeSize /*= 64*/)
 {
 	std::vector<Vector4> UniformSampleDirectionsOnSphere;
 	UniformSampleDirectionsOnSphere.resize(square(vDegreeSize));
@@ -142,9 +86,8 @@ void App::__createSphereSampler(int vDegreeSize)
 		}
 	}
 
-	m_SphereSamplerTexture = Texture::fromMemory("SphereSampler", UniformSampleDirectionsOnSphere.data(), ImageFormat::RGBA32F(), vDegreeSize, vDegreeSize, 1, 1, ImageFormat::RGBA16F(), Texture::DIM_2D, false);
+	return Texture::fromMemory("SphereSampler", UniformSampleDirectionsOnSphere.data(), ImageFormat::RGBA32F(), vDegreeSize, vDegreeSize, 1, 1, ImageFormat::RGBA16F(), Texture::DIM_2D, false);
 }
-
 
 void App::__makeGUI()
 {
@@ -152,24 +95,64 @@ void App::__makeGUI()
 	showRenderingStats = false;
 }
 
-void App::__initLightFiledSurface(const AABox& vBoundingBox)
+void App::__precomputeLightFiledSurface(SLightFieldSurface& vioLightFieldSurface)
 {
-	auto BoundingBoxRange = vBoundingBox.high() - vBoundingBox.low();
+	shared_ptr<Texture> RadianceCubemap = Texture::createEmpty("RadianceCubemap", 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_CUBE_MAP);
+	shared_ptr<Texture> DistanceCubemap = Texture::createEmpty("DistanceCubemap", 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_CUBE_MAP);
 
-	m_LightFiledSurface.ProbeCounts = Vector3int32(4, 4, 4);//TODO
-	m_LightFiledSurface.ProbeSteps = BoundingBoxRange / m_LightFiledSurface.ProbeCounts;
-	m_LightFiledSurface.ProbeStartPosition = vBoundingBox.low();
+	shared_ptr<Framebuffer> LightFiledFramebuffer = Framebuffer::create("LightFiledFB");
 
-	m_LightFiledSurface.RadianceProbeGrid = Texture::createEmpty("RaidanceProbeGrid", 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_2D_ARRAY, false, 2);
-	m_LightFiledSurface.IrradianceProbeGrid = Texture::createEmpty("IrraidanceProbeGrid", 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_CUBE_MAP_ARRAY);
+	for (int i = 0; i < m_ProbePositionSet.size(); ++i)
+	{
+		__renderLightFiledProbe(i, RadianceCubemap, DistanceCubemap);
+
+		LightFiledFramebuffer->set(Framebuffer::COLOR0, vioLightFieldSurface.RadianceProbeGrid, CubeFace::POS_X, 0, i);
+
+		renderDevice->push2D(LightFiledFramebuffer); {
+			renderDevice->setBlendFunc(RenderDevice::BLEND_ONE, RenderDevice::BLEND_ZERO);
+			auto Sampler = Sampler::cubeMap();
+			Sampler.interpolateMode = InterpolateMode::BILINEAR_NO_MIPMAP;
+
+			Args args;
+			args.setRect(renderDevice->viewport());
+			args.setUniform("ProbeColorCubemap", RadianceCubemap, Sampler);
+			args.setUniform("NumSamples", 2048);
+			args.setUniform("LobeSize", 1.0f);
+			args.setUniform("SphereSampler", m_SphereSamplerTexture, Sampler::buffer());
+
+			LAUNCH_SHADER("ComputeProbeGrid.pix", args);
+		} renderDevice->pop2D();
+	}
 }
 
-
-void App::__placeProbe()
+SLightFieldSurface App::__initLightFiledSurface()
 {
-	auto ProbeCounts = m_LightFiledSurface.ProbeCounts;
-	auto ProbeSteps = m_LightFiledSurface.ProbeSteps;
-	auto ProbeStartPos = m_LightFiledSurface.ProbeStartPosition;
+	SLightFieldSurface LightFieldSurface;
+
+	AABox BoundingBox;
+	Surface::getBoxBounds(m_posed3D, BoundingBox);
+
+	auto BoundingBoxRange = BoundingBox.high() - BoundingBox.low();
+
+	LightFieldSurface.ProbeCounts = Vector3int32(4, 4, 4);//TODO
+	LightFieldSurface.ProbeSteps = BoundingBoxRange / LightFieldSurface.ProbeCounts;
+	LightFieldSurface.ProbeStartPosition = BoundingBox.low();
+
+	auto ProbeNum = LightFieldSurface.ProbeCounts.x * LightFieldSurface.ProbeCounts.y * LightFieldSurface.ProbeCounts.z;
+
+	LightFieldSurface.RadianceProbeGrid = Texture::createEmpty("RaidanceProbeGrid", 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_2D_ARRAY, false, ProbeNum);
+	LightFieldSurface.IrradianceProbeGrid = Texture::createEmpty("IrraidanceProbeGrid", 1024, 1024, ImageFormat::RGB32F(), Texture::DIM_2D_ARRAY, false, ProbeNum);
+
+	return LightFieldSurface;
+}
+
+std::vector<Vector3> App::__placeProbe(const SLightFieldSurface& vLightFiledSurface)
+{
+	std::vector<Vector3> ProbePositionSet;
+
+	auto ProbeCounts   = vLightFiledSurface.ProbeCounts;
+	auto ProbeSteps    = vLightFiledSurface.ProbeSteps;
+	auto ProbeStartPos = vLightFiledSurface.ProbeStartPosition;
 
 	for (int i = 0; i < ProbeCounts.z; ++i)
 	{
@@ -177,7 +160,7 @@ void App::__placeProbe()
 		{
 			for (int k = 0; k < ProbeCounts.x; ++k)
 			{
-				m_ProbePositionSet.emplace_back(Vector3(
+				ProbePositionSet.emplace_back(Vector3(
 					ProbeStartPos.x + k * ProbeSteps.x,
 					ProbeStartPos.y + j * ProbeSteps.y,
 					ProbeStartPos.z + i * ProbeSteps.z
@@ -185,9 +168,11 @@ void App::__placeProbe()
 			}
 		}
 	}
+
+	return ProbePositionSet;
 }
 
-void App::__renderLightFiledProbe(uint32 vProbeIndex, Array<shared_ptr<Texture>>& vRadianceTexture, Array<shared_ptr<Texture>>& vDistanceTexture)
+void App::__renderLightFiledProbe(uint32 vProbeIndex, shared_ptr<Texture> voRadianceCubemap, shared_ptr<Texture> voDistanceCubemap)
 {
 	Array<shared_ptr<Surface> > surface;
 	{
@@ -223,8 +208,8 @@ void App::__renderLightFiledProbe(uint32 vProbeIndex, Array<shared_ptr<Texture>>
 	CFrame cframe = camera->frame();
 
 	setActiveCamera(camera);
-	for (int face = 0; face < 6; ++face) {
-		Texture::getCubeMapRotation(CubeFace(face), cframe.rotation);
+	for (int Face = 0; Face < 6; ++Face) {
+		Texture::getCubeMapRotation(CubeFace(Face), cframe.rotation);
 		camera->setFrame(cframe);
 
 		renderDevice->setProjectionAndCameraMatrix(activeCamera()->projection(), activeCamera()->frame());
@@ -233,22 +218,13 @@ void App::__renderLightFiledProbe(uint32 vProbeIndex, Array<shared_ptr<Texture>>
 		onGraphics3D(renderDevice, surface);
 		onGraphics3D(renderDevice, surface);
 		m_osWindowHDRFramebuffer->get(Framebuffer::DEPTH);
-		Texture::copy(m_osWindowHDRFramebuffer->texture(0), vRadianceTexture[face], 0, 0, 1,
-					  Vector2int16((m_osWindowHDRFramebuffer->texture(0)->vector2Bounds() - vRadianceTexture[face]->vector2Bounds()) / 2.0f),
-					  CubeFace::POS_X, CubeFace::POS_X, nullptr, false);
+		Texture::copy(m_osWindowHDRFramebuffer->texture(0), voRadianceCubemap, 0, 0, 1,
+					  Vector2int16((m_osWindowHDRFramebuffer->texture(0)->vector2Bounds() - voRadianceCubemap->vector2Bounds()) / 2.0f),
+					  CubeFace::POS_X, CubeFace(Face) , nullptr, false);
 
-
-		Texture::copy(m_gbuffer->texture(GBuffer::Field::CS_POSITION), vDistanceTexture[face], 0, 0, 1,
-					  Vector2int16((m_osWindowHDRFramebuffer->texture(0)->vector2Bounds() - vDistanceTexture[face]->vector2Bounds()) / 2.0f),
-					  CubeFace::POS_X, CubeFace::POS_X, nullptr, false);
-
-		//Gbuffer
-		//m_ProbeGBuffer->prepare(renderDevice, camera, 0, -(float)previousRealTimeStep(), Vector2int16(0, 0), Vector2int16(0, 0));
-		//m_ProbeGBuffer->setCamera(camera);
-		//m_gbuffer->prepare(renderDevice, camera, 0, -(float)previousSimTimeStep(), m_settings.hdrFramebuffer.depthGuardBandThickness, m_settings.hdrFramebuffer.colorGuardBandThickness);
-
-		//__computeGBuffers(renderDevice, surface);
-
+		Texture::copy(m_gbuffer->texture(GBuffer::Field::CS_POSITION), voDistanceCubemap, 0, 0, 1,
+					  Vector2int16((m_osWindowHDRFramebuffer->texture(0)->vector2Bounds() - voDistanceCubemap->vector2Bounds()) / 2.0f),
+					  CubeFace::POS_X, CubeFace(Face), nullptr, false);
 	}
 
 	setActiveCamera(oldCamera);
--- LightFiledProbe/App.h
+++ LightFiledProbe/App.h
@@ -15,27 +15,24 @@ struct SLightFieldSurface
 class App : public GApp
 {
 	shared_ptr<Framebuffer> m_ProbeGBuffer;
-	shared_ptr<Framebuffer> m_LightFiledFB;
 
 	SLightFieldSurface   m_LightFiledSurface;
 	std::vector<Vector3> m_ProbePositionSet;
 
 	shared_ptr<Texture>  m_SphereSamplerTexture;
-	shared_ptr<Texture>  m_pCubemapColor;
-	shared_ptr<Texture>  m_pCubemapDistance;
 
-	bool m_IsInit = false;
+	bool m_IsPrecomputed = false;
 public:
 	App(const GApp::Settings& vSettings) : GApp(vSettings) {}
 
 	virtual void onInit() override;
 	virtual void onGraphics3D(RenderDevice* rd, Array<shared_ptr<Surface> >& surface) override;
 
-
 private:
 	void __makeGUI();
-	void __createSphereSampler(int vDegreeSize);
-	void __initLightFiledSurface(const AABox& vBoundingBox);
-	void __placeProbe();
-	void __renderLightFiledProbe(uint32 vProbeIndex, Array<shared_ptr<Texture>>& vRadianceTexture, Array<shared_ptr<Texture>>& vDistanceTexture);
+	void __precomputeLightFiledSurface(SLightFieldSurface& vioLightFiledSurface);
+	shared_ptr<Texture>  __createSphereSampler(int vDegreeSize = 64);
+	SLightFieldSurface   __initLightFiledSurface();
+	std::vector<Vector3> __placeProbe(const SLightFieldSurface& vLightFiledSurface);
+	void __renderLightFiledProbe(uint32 vProbeIndex, shared_ptr<Texture> voRadianceCubemap, shared_ptr<Texture> voDistanceCubemap);
 };
