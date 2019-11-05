be10c2c8453e3e6801b303240651d8b3a1c8d142
Vicent Chen <121912419@qq.com>
2019-11-05 13:01:36 +0800

    update ui

--- LightFiledProbe/App.cpp
+++ LightFiledProbe/App.cpp
@@ -9,9 +9,9 @@ void App::onInit()
 
 	__specifyGBufferEncoding();
 
-	//loadScene("Simple Cornell Box");
+	loadScene("Simple Cornell Box");
 	//loadScene("Simple Cornell Box (Mirror)");
-	loadScene("Simple Cornell Box (No Little Boxes)");
+	//loadScene("Simple Cornell Box (No Little Boxes)");
 
 	//loadScene("Sponza (Glossy Area Lights)");
 	//loadScene("Sponza (Statue Glossy)");
